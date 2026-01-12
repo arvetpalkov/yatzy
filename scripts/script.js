38cb70cb2b92aa3fbc2b2b0ca625fe69743f9184
arvetpalkov <arvet.palkov@gmail.com>
2026-01-13 00:49:58 +0200

    add roll controll and select buttons hide

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
+const rollInfo = document.querySelector('.roll-info');
 const diceBoxes = document.querySelectorAll('.dice-box');
 const statusIndicators = document.querySelectorAll('.status-indicator');
 const dices = document.querySelectorAll('.dice');
 const selectButtons = document.querySelectorAll('.select-button');
 const rollButton = document.querySelector('.roll-button');
+
+let rollNumber = 1;
+let canHold = false;
 
 const diceValues = ['', '', '', '', ''];
 const diceToHold = ['', '', '', '', ''];
@@ -16,6 +20,7 @@ for (let i = 0; i < 5; i++) {
 rollButton.addEventListener('click', rollDices);
 
 function toggleHold(i) {
+  if (!canHold) return;
   if (diceToHold[i]) {
     diceToHold[i] = '';
   } else {
@@ -24,7 +29,33 @@ function toggleHold(i) {
   updateHoldView(diceToHold);
 }
 
+function removeAllHolds() {
+  diceToHold.forEach((val, i, arr) => {
+    arr[i] = '';
+  })
+  updateHoldView(diceToHold);
+}
+
+function rollControl() {
+  if (rollNumber === 1) {
+    rollInfo.innerHTML = '1st ROLL';
+    canHold = false;
+    removeAllHolds();
+    hideSelectButtons();
+    rollNumber++;
+  } else if (rollNumber === 2) {
+    rollInfo.innerHTML = '2nd ROLL';
+    canHold = true;
+    showSelectButtons();
+    rollNumber++;
+  } else {
+    rollInfo.innerHTML = 'LAST ROLL';
+    rollNumber = 1;
+  }
+}
+
 function rollDices() {
+  rollControl();
   for (let i = 0; i < 5; i++) {
     if (!diceToHold[i]) {
       diceValues[i] = rollDice();
@@ -59,4 +90,16 @@ function displayDiceValues(diceValues) {
   dices.forEach((diceElement, i) => {
     diceElement.innerHTML = diceValues[i];
   });
+}
+
+function hideSelectButtons() {
+  selectButtons.forEach((button) => {
+    button.classList.add('hide');
+  })
+}
+
+function showSelectButtons() {
+  selectButtons.forEach((button) => {
+    button.classList.remove('hide');
+  })
 }
